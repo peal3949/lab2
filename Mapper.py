c91f41af8d74964c852054a06be6849c42b785f7
peal3949 <pedro.AlbuquerqueLemos@colorado.edu>
2025-09-05 10:51:17 -0600

    Update Mapper.py

--- Mapper.py
+++ Mapper.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
-"""mapper.py"""
-
 import sys
+import re
 
-# input comes from STDIN (standard input)
+# Regular expression to match URLs (http or https)
+# url_pattern = re.compile(r'https?://[^\s"\'<>]+')
+url_pattern = re.compile(r'href=\"(.*?)\"')
+
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
-    # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
-        print('%s\t%s' % (word, 1))
+    # Find all URLs in the line
+    urls = url_pattern.findall(line)
+    for url in urls:
+        print(f"{url}\t1")
+    
