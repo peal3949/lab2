15eb50d7d720b442ab0acf79b17dae3beb7103e1
peal3949 <pedro.AlbuquerqueLemos@colorado.edu>
2025-09-05 10:51:37 -0600

    Update Reducer.py

--- Reducer.py
+++ Reducer.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python
-"""reducer.py"""
-
-from operator import itemgetter
+#!/usr/bin/env python3
 import sys
 
-current_word = None
+current_url = None
 current_count = 0
-word = None
+url = None
 
-# input comes from STDIN
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
-
-    # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
-
-    # convert count (currently a string) to int
     try:
+        url, count = line.split('\t', 1)
         count = int(count)
     except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
+        # Skip malformed lines
         continue
 
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
+    if current_url == url:
         current_count += count
     else:
-        if current_word:
-            # write result to STDOUT
-            print('%s\t%s' % (current_word, current_count))
+        if current_url:
+            if current_count > 5:
+                print(f"{current_url}\t{current_count}")
         current_count = count
-        current_word = word
-            
-# do not forget to output the last word if needed!
-if current_word == word:
-    print('%s\t%s' % (current_word, current_count))
+        current_url = url
+
+# Don’t forget the last one
+if current_url == url:
+    if current_count > 5:
+        print(f"{current_url}\t{current_count}")
